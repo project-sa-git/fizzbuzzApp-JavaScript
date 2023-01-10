7dc4ac0df18d73640fb5975fe7cdf3fed7d2139a
mymac <trybook1901@gmail.com>
2023-01-10 22:14:19 +0900

    fix: レビューのコメント部分を修正

--- index.js
+++ index.js
@@ -15,42 +15,31 @@ function calculateFizzbuzz() {
   if (
     fizzNumForm === "" ||
     buzzNumForm === "" ||
-    Number.isInteger(Number(fizzNumForm)) == false ||
-    Number.isInteger(Number(buzzNumForm)) == false
+    !Number.isInteger(Number(fizzNumForm)) ||
+    !Number.isInteger(Number(buzzNumForm))
   ) {
-    let list = document.getElementById("list");
-    let li = document.createElement("li");
+    const list = document.getElementById("list");
+    const li = document.createElement("li");
     li.innerHTML = "整数値を入力してください";
     list.appendChild(li);
     return;
   }
 
-  // 大小並び替え
-  let smallNum = 0;
-  let largeNum = 0;
-  if (buzzNumForm < fizzNumForm) {
-    smallNum = buzzNumForm;
-    largeNum = fizzNumForm;
-  } else {
-    smallNum = fizzNumForm;
-    largeNum = buzzNumForm;
-  }
-
   // FizzBuzz判定
   for (let i = 1; i < 100; i++) {
-    if (i % (smallNum * largeNum) === 0) {
-      let list = document.getElementById("list");
-      let li = document.createElement("li");
+    if (i % (fizzNumForm * buzzNumForm) === 0) {
+      const list = document.getElementById("list");
+      const li = document.createElement("li");
       li.innerHTML = "FizzBuzz " + i;
       list.appendChild(li);
-    } else if (i % largeNum === 0) {
-      let list = document.getElementById("list");
-      let li = document.createElement("li");
+    } else if (i % buzzNumForm === 0) {
+      const list = document.getElementById("list");
+      const li = document.createElement("li");
       li.innerHTML = "Buzz " + i;
       list.appendChild(li);
-    } else if (i % smallNum === 0) {
-      let list = document.getElementById("list");
-      let li = document.createElement("li");
+    } else if (i % fizzNumForm === 0) {
+      const list = document.getElementById("list");
+      const li = document.createElement("li");
       li.innerHTML = "Fizz " + i;
       list.appendChild(li);
     }
@@ -64,7 +53,7 @@ function initDeleteTag() {
   //[li]の数を取得する
   const len = ul.children.length;
   //[li]の数だけ繰り返す
-  for (var i = 0; i < len; i++) {
+  for (let i = 0; i < len; i++) {
     //[li]を削除する
     ul.removeChild(ul.children[0]);
   }
